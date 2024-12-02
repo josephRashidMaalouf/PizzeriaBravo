bf25b3465b9e05db5dbe75b0bf0a6c5525816b0b
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-12-02 17:36:54 +0100

    add result to dto mapper

---
+++ FoodStuffService/Dtos/Mappers/ResultMapper.cs
@@ -0,0 +1,17 @@
+﻿using FoodStuffService.Domain.Entities;
+using FoodStuffService.Domain.Models;
+
+namespace FoodStuffService.Dtos.Mappers;
+
+public static class ResultMapper
+{
+    public static ResultDto<FoodStuff> ToDto(this Result<FoodStuff> result)
+    {
+        return new ResultDto<FoodStuff>
+        {
+            Data = result.Data,
+            Message = result.Message,
+            IsSuccess = result.IsSuccess,
+        };
+    }
+}
