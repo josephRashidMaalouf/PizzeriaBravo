c4a229151e25bc03cf90a84b4e400635fca02fd4
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-12-02 15:56:53 +0100

    Create interfaces

---
+++ FoodStuffService.Domain/Interfaces/ICrud.cs
@@ -0,0 +1,13 @@
+﻿using FoodStuffService.Domain.Entities;
+using FoodStuffService.Domain.Models;
+
+namespace FoodStuffService.Domain.Interfaces;
+
+public interface ICrud<T> where T : EntityBase
+{
+    Task<Result<IEnumerable<T>>> GetAllAsync();
+    Task<Result<T>> GetByIdAsync(Guid id);
+    Task<Result<T>> CreateAsync(T foodStuff);
+    Task<Result<T>> UpdateAsync(Guid id, T foodStuff);
+    Task<Result<bool>> DeleteAsync(Guid id);
+}
---
+++ FoodStuffService.Domain/Interfaces/IFoodStuffService.cs
@@ -0,0 +1,8 @@
+﻿using FoodStuffService.Domain.Entities;
+
+namespace FoodStuffService.Domain.Interfaces;
+
+public interface IFoodStuffService : ICrud<FoodStuff>
+{
+    
+}
