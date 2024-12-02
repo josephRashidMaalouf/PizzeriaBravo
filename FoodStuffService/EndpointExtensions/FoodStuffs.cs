6c3b74f972ede1f0e3ff229b0cef5fcee37fc62a
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-12-02 16:37:11 +0100

    start writing FoodStuff endpoints

---
+++ FoodStuffService/EndpointExtensions/FoodStuffs.cs
@@ -0,0 +1,22 @@
+﻿using FoodStuffService.Domain.Interfaces;
+
+namespace FoodStuffService.EndpointExtensions;
+
+public static class FoodStuffs
+{
+    public static IEndpointRouteBuilder MapEndPoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGroup("api/food-stuffs");
+
+        app.MapGet("", GetAll)
+            .WithName("FoodStuffs");
+        
+        return app;
+    }
+
+    private static async Task<IResult> GetAll(IFoodStuffService foodStuffService)
+    {
+        
+        
+    }
+}
