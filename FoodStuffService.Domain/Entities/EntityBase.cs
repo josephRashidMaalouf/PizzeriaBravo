b9f2bd88a9d48271006ecac351d96b3eb04c4cb7
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-12-02 18:05:11 +0100

    Fix Guid serialsation

--- FoodStuffService.Domain/Entities/EntityBase.cs
+++ FoodStuffService.Domain/Entities/EntityBase.cs
@@ -1,9 +1,11 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace FoodStuffService.Domain.Entities;
 
 public class EntityBase
 {
     [BsonId]
+    [BsonRepresentation(BsonType.String)] 
     public Guid Id { get; set; } = Guid.NewGuid();
 }
